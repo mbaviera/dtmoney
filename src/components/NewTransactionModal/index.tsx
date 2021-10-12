e3b723155e0df3a1f086c8dae146754860c488ef
Mateus <mateusmtu@gmail.com>
2021-10-12 14:47:50 -0300

    modal ok

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,50 @@
 import Modal from 'react-modal'
+import { Container, TransactionTypeContainer } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
-interface NewTransactionModalProps{
+interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){//pega as props desestruturando
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {//pega as props desestruturando
     return (
         <Modal
             isOpen={isOpen}
-            onRequestClose={onRequestClose}>
-            <h2>Cadatrar Transacao</h2>
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content">
+
+            <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <img src={closeImg} alt="fechar modal" />
+            </button>
+
+            <Container>
+                <h2>Cadastrar Transação</h2>
+
+                <input placeholder="Titulo" />
+                <input placeholder="Valor" type="number" />
+
+                <TransactionTypeContainer > 
+                    <button type="button"> 
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+
+                    <button type="button"> 
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>  
+
+                <input placeholder="Categoria" />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     );
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,91 @@
 import styled from "styled-components";
+import { darken } from "polished";
 
-export const Container = styled.div`
+export const Container = styled.form`
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
 
+  input {
+    width: 100%;
+    padding: 0.5rem;
+    height: 4rem;
+    border-radius: 0.25rem;
+    background: #e7e9ee;
+    border: 1px solid #d7d7d7;
+
+    font-weight: 400;
+    font-size: 1rem;
+
+    &::placeholder {
+      //mudar a cor do titulo do placeholder do input
+      color: var(--text-body);
+    }
+
+    & + input {
+      //todo input que tiver um input antes dele recebe o margin (ou seja a partir do segundo)
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+    //estiliza so o button com tipo submit
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    background: var(--green);
+    color: #fff;
+    border-radius: 0.25rem;
+    border: 0;
+    font-size: 1rem;
+    margin-top: 1.5rem;
+    font-weight: 600;
+
+    transition: filter 0.2s;
+
+    &:hover {//quando passar o mouse em cima do button diminui a luminosidade pra 0.9
+        filter: brightness(0.9);
+    }
+  }
+`;
+
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
+
+    button {
+        height: 4rem;
+        border: 1px solid #d7d7d7;
+        border-radius: 0.25rem;
+
+        background: transparent;
+        display: flex;
+
+        align-items: center;
+        justify-content: center;
+
+        transition: border-color 0.2s;
+
+        &:hover {
+            border-color: ${darken(0.1, '#d7d7d7')};//escure a cor em 10% com essa lib ai
+        }
+
+        img {
+            width: 20px;
+            height: 20px;
+        }
+
+        span {
+            display: inline-block;
+            margin-left: 1rem;
+            font-size: 1rem;
+            color: var(--text-title);
+
+        }
+    }
 `
